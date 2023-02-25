a7674a64705a9567e528ca49ce8b034f6916e70b
Fer Longo <fernandolongo137@gmail.com>
2023-02-25 20:14:43 -0300

    500 to 400

--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -1,5 +1,5 @@
 import { Response } from "express";
 
 export const handleHttp = (res: Response, error: any) => {
-  res.status(500).send({ ok: false, msg: "" + error });
+  res.status(400).send({ ok: false, msg: "" + error });
 };
